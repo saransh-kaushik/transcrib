be755c25527718abfe9fc57e92c5690fd4eaba51
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-09-28 09:32:14 +0530

    Initial Commit

---
+++ transcribe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  transcribe
+//
+//  Created by Hompushparaj Mehta on 28/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
