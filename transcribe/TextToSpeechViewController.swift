f1def49a223b6c46ebc52cf256e060d4c91455de
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-09-30 10:55:01 +0530

    Feat: Add placeholder functionality

--- transcribe/TextToSpeechViewController.swift
+++ transcribe/TextToSpeechViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class TextToSpeechViewController: UIViewController {
+class TextToSpeechViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var TextInputTextView: UITextView!
+    
+    let placeholderText = "Enter your text here....."
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,8 +20,31 @@ class TextToSpeechViewController: UIViewController {
         TextInputTextView.layer.borderWidth = 1
         TextInputTextView.layer.cornerRadius = 10
         TextInputTextView.clipsToBounds = true
+//        TextInputTextView.becomeFirstResponder()
+        
+        // Setting initial placeholder
+        TextInputTextView.text = placeholderText
+        TextInputTextView.textColor = .lightGray
+        
+        // Setting the delegate to handle text view events
+        TextInputTextView.delegate = self
     }
     
+    
+    // Method to remove placeholder when user start typing
+    func textViewDidBeginEditing(_ TextInputTextView: UITextView) {
+        if TextInputTextView.text == placeholderText{
+            TextInputTextView.text = ""
+            TextInputTextView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ TextInputTextView: UITextView) {
+        if TextInputTextView.text.isEmpty {
+            TextInputTextView.text = placeholderText
+            TextInputTextView.textColor = UIColor.lightGray
+        }
+    }
 
     /*
     // MARK: - Navigation
