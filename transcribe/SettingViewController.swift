2fd398015981682aec7d55c33b4f4615bfd21b82
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-10-01 23:41:25 +0530

    Add SettingViewController: Created main settings interface with multiple user options.

--- transcribe/SettingViewController.swift
+++ transcribe/SettingViewController.swift
@@ -7,23 +7,86 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Settings options
+    let settingsOptions = [
+        "Language Preferences", "Voice Preferences (for TTS)", "History Management",
+        "Data Privacy", "Sound Setting", "Appearance Setting",
+        "Notifications", "About", "Feedback"
+    ]
+    let settingImage = [
+        "globe.americas","speaker.wave.3","clock","lock.shield","speaker.slash","moon.circle","bell","info.circle","exclamationmark.bubble"
+    ]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let nib = UINib.init(nibName: "SettingTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "SettingCell")
+        
+        // Set the delegate and data source
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+extension SettingViewController {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingsOptions.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell",for: indexPath) as?
+        SettingTableViewCell
+        
+        let name = settingsOptions[indexPath.row]
+        
+        let systemImageName = settingImage[indexPath.row]
+        
+        let image = UIImage(systemName: systemImageName) ?? UIImage(systemName: "questionmark.circle")
+        let arrow = UIImage(systemName: "chevron.right")
+        
+        cell?.commonInit(name, image!, arrow!)
+        
+        return cell!
+        
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            let selectedSetting = settingsOptions[indexPath.row]
+
+            // Navigate to the corresponding ViewController based on the selected setting
+            var destinationVC: UIViewController?
+
+            switch selectedSetting {
+            case "Language Preferences":
+                destinationVC = LanguagePreferenceViewController()
+            case "Voice Preferences (for TTS)":
+                destinationVC = VoicePreferenceViewController()
+            case "History Management":
+                destinationVC = HistoryManagementViewController()
+            case "Data Privacy":
+                destinationVC = DataPrivacyViewController()
+            case "Sound Setting":
+                destinationVC = SoundSettingViewController()
+            case "Appearance Setting":
+                destinationVC = AppearanceSettingViewController()
+            case "Notifications":
+                destinationVC = NotificationsViewController()
+            case "About":
+                destinationVC = AboutViewController()
+            case "Feedback":
+                destinationVC = FeedbackViewController()
+            default:
+                break
+            }
+            
+            if let destinationVC = destinationVC {
+                navigationController?.pushViewController(destinationVC, animated: true)
+            }
+        }
+    }
