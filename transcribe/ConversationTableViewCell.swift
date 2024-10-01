c8d46094ecf5982e867e570c7b4d4fb86c8925b0
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-10-01 23:41:25 +0530

    Rename CustomTableViewCell to ConversationTableViewCell: Refactored to improve clarity of its usage.

--- transcribe/ConversationTableViewCell.swift
+++ transcribe/ConversationTableViewCell.swift
@@ -8,16 +8,31 @@
 import UIKit
 
 class ConversationTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var conversationImage: UIImageView!
+    @IBOutlet weak var conversationTitle: UILabel!
+    
+    @IBOutlet weak var conversationSubtitle: UILabel!
+    
+    @IBOutlet weak var conversationTime: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.conversationImage.layer.cornerRadius = conversationImage.frame.size.width / 2
+        conversationImage.clipsToBounds = true
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    func commonInit(_ name: String,_ subtitle: String,_ timestamp:String, _ image: UIImage) {
+        conversationTitle.text = name
+        conversationSubtitle.text = subtitle
+        conversationTime.text = timestamp
+        conversationImage.image = image
     }
     
 }
